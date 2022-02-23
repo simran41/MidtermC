6ee2528cc4322e82903efac1a2655a7f1648e9cf
simran41 <kau10773@sheridancollege.ca>
2022-02-23 07:24:40 -0800

    commit to card.java file

---
+++ src/TestUserProfile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class TestUserProfile {
+     public static void main(String[] args) {
+        
+         genre g1 = new genre();
+         System.out.println("Enter your name");
+         System.out.println("Choose your favorite genre");
+         System.out.println("  JAZZ, HIPHOP, POP, ROCK ");
+        
+         System.out.println("your profile has been created");
+    }
+}
---
+++ src/genre.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class genre {
+    public enum choosegenre{
+        JAZZ, HIPHOP, POP, ROCK;
+    }
+     
+}
